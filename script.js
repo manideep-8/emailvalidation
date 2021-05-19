100a16ab1c53b475ac6b0edf0bb5d6c27c568869
manideep-8 <60574685+manideep-8@users.noreply.github.com>
2021-05-19 14:55:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function validate() {
+
+  var a= document.getElementById('em1').value;
+  var b= document.getElementById('em2').value;
+
+if (a!==b) {
+
+  alert("please enter correct mail")
+
+} else {
+
+  alert("ok next step")
+
+}
+}
